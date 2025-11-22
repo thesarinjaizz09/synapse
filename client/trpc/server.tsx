3fd12f7d370da185c2f95effdbf6a2e125395495
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-11-22 21:38:07 +0530

    feat: added prefetch and hydration functions

--- client/trpc/server.tsx
+++ client/trpc/server.tsx
@@ -3,7 +3,8 @@ import { cache } from 'react';
 import { createTRPCContext } from './init';
 import { appRouter } from './routers/_app';
 import { makeQueryClient } from './query-client';
-import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
+import { createTRPCOptionsProxy, TRPCQueryOptions } from '@trpc/tanstack-react-query';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
 export const getQueryClient = cache(makeQueryClient);
 export const trpc = createTRPCOptionsProxy({
@@ -11,3 +12,24 @@ export const trpc = createTRPCOptionsProxy({
   router: appRouter,
   queryClient: getQueryClient,
 });
+
+export const caller = appRouter.createCaller(createTRPCContext());
+
+export function HydrateClient(props: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {props.children}
+    </HydrationBoundary>
+  );
+}
+export function prefetch<T extends ReturnType<TRPCQueryOptions<any>>>(
+  queryOptions: T,
+) {
+  const queryClient = getQueryClient();
+  if (queryOptions.queryKey[1]?.type === 'infinite') {
+    void queryClient.prefetchInfiniteQuery(queryOptions as any);
+  } else {
+    void queryClient.prefetchQuery(queryOptions);
+  }
+}
