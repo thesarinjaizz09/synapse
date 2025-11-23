27ec2aba58e5ea737df71fac77284111ab8d5bb7
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-11-23 19:09:51 +0530

    refactor: minor changes

--- client/app/(user-dashboard)/(with-sidebar)/layout.tsx
+++ client/app/(user-dashboard)/(with-sidebar)/layout.tsx
@@ -5,15 +5,12 @@ import {
 } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
-import { isAuthenticated } from "@/lib/auth/utils";
 
 export default async function UserDashboardLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    await isAuthenticated();
-    
     return (
         <div className="[--header-height:calc(--spacing(14))]">
             <SidebarProvider className="flex flex-col">
