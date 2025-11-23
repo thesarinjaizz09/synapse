3bf6920c5da5abd377d92cef70eb916dc21423c8
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-11-23 23:43:42 +0530

    feat: added 'use client'

--- client/features/workflows/components/workflows.tsx
+++ client/features/workflows/components/workflows.tsx
@@ -1,16 +1,15 @@
+'use client'
+
 import { useSuspenseWorkflows } from "../hooks/use-workflows"
 
 const WorkflowsContainer = () => {
     const workflows = useSuspenseWorkflows()
-    
+
     return (
         <div>
-            {workflows.data?.workflows.map((workflow) => (
-                <div key={workflow.id}>
-                    <h2>{workflow.name}</h2>
-                    <p>Status: {workflow.status}</p>
-                </div>
-            ))}
+            <p>
+                {JSON.stringify(workflows.data.workflows, null, 2)}
+            </p>
         </div>
     )
 }
