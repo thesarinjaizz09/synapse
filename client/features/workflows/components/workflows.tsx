9e39b2ef69020487da65cb39d4c15a6f2a3403e1
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-11-25 15:39:40 +0530

    chore: minor styling

--- client/features/workflows/components/workflows.tsx
+++ client/features/workflows/components/workflows.tsx
@@ -406,7 +406,7 @@ export const WorkflowsItem = ({ workflow }: { workflow: Workflow }) => {
 
     const actions = <div className="flex items-center justify-center gap-2">
         <div className={`border px-2 py-1.5 flex items-center gap-1 rounded-sm transition-transform duration-200 hover:rounded-lg hover:scale-102 ${themeClasses[workflow.status]}`}>
-            <p className={`text-[10.5px] tracking-wider`}>
+            <p className={`text-[10.8px] tracking-wider`}>
                 {workflow.status}
             </p>
         </div>
