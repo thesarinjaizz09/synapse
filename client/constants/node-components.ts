d89629f54d9b1eb092ef2b4a26f24059611d949f
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-11-25 20:26:36 +0530

    feat: added base and placeholder node

---
+++ client/constants/node-components.ts
@@ -0,0 +1,8 @@
+import { NodeType } from "@/lib/generated/prisma/enums";
+import { NodeTypes } from "@xyflow/react";
+
+export const NODE_COMPONENTS = {
+    [NodeType.INITIAL]: InitialNode,
+} as const satisfies NodeTypes
+
+export type RegisteredNodeTypes = keyof typeof NODE_COMPONENTS
